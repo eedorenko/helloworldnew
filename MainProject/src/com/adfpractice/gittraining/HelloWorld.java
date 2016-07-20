7452f64c52bb08bb15ca950ed8829a0f830972f4
Eugene Fedorenko <eugene.fedorenko@eproseed.com>
2016-07-20 14:25:11 +0200

    Initial commit

---
+++ MainProject/src/com/adfpractice/gittraining/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.adfpractice.gittraining;
+
+public class HelloWorld
+{
+  public HelloWorld()
+  {
+    super();
+  }
+
+  public void sayHello()
+  {
+    System.out.println("Hello World!!!");
+  }
+
+  public static void main(String[] args)
+  {
+    HelloWorld helloWorld = new HelloWorld();
+    helloWorld.sayHello();
+  }
+}
